ff9f28f5d51df45a7f63c2f5cc290f5fc92c309a
Lucas Silva <lucasbarboza299@gmail.com>
2024-07-03 12:01:18 -0300

    mudando a porta para 5000

--- app.py
+++ app.py
@@ -50,4 +50,4 @@ def gerar_resposta_pergunta(historico):
     return resposta_formatada
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
